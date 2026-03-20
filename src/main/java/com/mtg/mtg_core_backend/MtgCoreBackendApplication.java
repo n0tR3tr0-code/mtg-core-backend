e7bd604bcde9fef3c1280264c44acda384d3db64
Mattia Tartaglia <mattia.tartaglia@outlook.it>
2026-03-20 23:52:27 +0100

    Primo commit: Spring Boot Skeleton

---
+++ src/main/java/com/mtg/mtg_core_backend/MtgCoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mtg.mtg_core_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgCoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgCoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtg/mtg_core_backend/MtgCoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtg.mtg_core_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgCoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
